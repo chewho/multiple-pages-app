a4a85190e0e0054042a9283adbbf76b6ce0d1d19
chewho <molocrips@gmail.com>
2023-11-16 19:21:09 +0100

    feat: add an error page by errorElement prop

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
 
 import { MainNavigation } from "../components/MainNavigation";
 
-export const RootLayout = () => {
+export const ErrorPage = () => {
   return (
     <>
       <MainNavigation />
       <main>
-        <Outlet />
+        <h1>An error occured!</h1>
+        <p>Could not find this page!</p>
       </main>
     </>
   );
