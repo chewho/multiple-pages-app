774f9ad595507f65c0d585787f5e930f5ea66e0e
chewho <molocrips@gmail.com>
2023-11-20 17:15:31 +0100

    feat: add Links for dynamic routes

---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export const ProductDetailPage = () => {
+  const params = useParams();
+
+  return (
+    <>
+      <h1>Product Details!</h1>
+      <p>{params.productId}</p>
+    </>
+  );
+};
